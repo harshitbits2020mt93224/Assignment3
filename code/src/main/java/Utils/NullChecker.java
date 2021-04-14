0c0d2c83a61f3a258e48d5be28a36571e0a3a718
harshitsharma <harshitsharma@localhost>
2021-04-15 03:26:51 +0530

    Adding Utils File

---
+++ code/src/main/java/Utils/NullChecker.java
@@ -0,0 +1,21 @@
+
+
+public class NullChecker {
+
+ // Function to check if the String is Null
+ 
+    public static boolean isStringNull(String str)
+    {
+  
+        // Compare the string with null
+        // using == relational operator
+        // and return the result
+        if (str == null)
+            return true;
+        else
+            return false;
+    }
+
+
+
+}
